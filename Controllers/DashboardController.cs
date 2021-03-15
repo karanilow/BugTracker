ae7fb2301046434ad7949f7a33b7c0ecedd7fa6f
Nils <nils.turckheim@gmail.com>
2021-03-15 18:12:18 +1000

    End Week 4 - Design UI

---
+++ Controllers/DashboardController.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using bugtracker.Models;
+
+namespace bugtracker.Controllers
+{
+    public class DashboardController : Controller
+    {
+        private readonly ILogger<DashboardController> _logger;
+
+        public DashboardController(ILogger<DashboardController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+
+    }
+}
