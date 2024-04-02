1935c04351ec98039f43448c562a7b1918d77f14
Nils_de_Turckheim <nils.turckheim@gmail.com>
2024-04-02 13:25:43 +0200

    Secure appSettings

--- obj/Debug/net8.0/bugtracker.AssemblyInfo.cs
+++ obj/Debug/net8.0/bugtracker.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("bugtracker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+01316deadd9437bbd433ee6060042ab9b0b40a28")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d329a2233420c895ff2f94a5c182a9ada9402fef")]
 [assembly: System.Reflection.AssemblyProductAttribute("bugtracker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("bugtracker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
